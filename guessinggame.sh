f9a92014041e186e1cf36a577da1b8479e9db632
sridharanyak <sridharanya1998@gmail.com>
2020-07-27 18:48:08 +0530

    Second commit

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-read -p "Guess no.of files in a folder: " usrinput
-echo  "User guess is : $usrinput"
-a=`ls -l | wc -l`;
-if [ $a -gt $usrinput ]
-then
-       echo "Guess is too lower"
-elif [ $a -eq $usrinput ]
-then
-        echo "Congratulation guessed answer is correct"
-else
-        echo "Guess is too higher"
-fi
+call_guessfile ( )
+{
+        a=`ls -l | wc -l`;
+while read -p  "Guess no of files in a directory: " usrinput
+do
+if [ $a -lt $usrinput ]
+        then
+                        echo "Guess is too higher"
+                elif [ $a -eq $usrinput ]
+                then
+                                echo "Congratulation guessed answer is correct"
+                                        exit;
+                                else
+                                                echo "Guess too lower"
+
+        fi
+done
+}
+call_guessfile
